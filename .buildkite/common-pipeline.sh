e5babd7d3e469a4e3fe9a6216f52e54188d8dd8c
vsk <vsk@example.com>
2023-01-28 15:34:24 +0000

    added shell check

--- .buildkite/common-pipeline.sh
+++ .buildkite/common-pipeline.sh
@@ -3,9 +3,15 @@
 set -eu
 
 trivy_plugin="v1.18.0"
+shell_plugin="v1.3.0"
 
 echo "steps:"
 
 echo "  - command: ls"
 echo "    plugins:"
 echo "      - equinixmetal-buildkite/trivy#${trivy_plugin}:"
+
+echo "  - label: \":sparkles: SHELL CHECK\""
+echo "    plugins:"
+echo "      - shellcheck#${shell_plugin}:"
+echo "          files: script.sh"
